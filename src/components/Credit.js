f896d50553a4d72381eeaed14c3d0bc0ff0080d1
Punit <jpunit2005@gmail.com>
2024-10-05 20:00:51 +0530

    Update

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ReactAnimatedWeather from "react-animated-weather";
 
-export default function Credit() {
+function Forecast({ weather }) {
+
+
   return (
-    <div class="credit">
-    <p>
-      Coded by
-      <a href="https://www.shecodes.io/graduates/73033-chaimae-lamirine"
-        > Chaimae Lamirine</a
-      >, <a href="https://github.com/shemmee/React-Weather-App">Open sourced on Github.</a> Hosted on Netlify
-    </p>
-  </div>
+    <div>
+    </div>
   );
-}
+}        
+
+export default Forecast;
